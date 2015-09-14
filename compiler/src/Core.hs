6f43d2c37ea90f2d04eb89848f351134cf088812
UNNO Hideyuki <unno.hideyuki@nifty.com>
2015-09-14 19:41:08 +0900

    data types for Core language.

---
+++ compiler/src/Core.hs
@@ -0,0 +1,57 @@
+module Core where
+
+import Symbol
+
+data Module = Module Id [TypeDefn] [ValueDefn]
+
+data TypeDefn = DataDefn Id [TypeBinder] [ConstrDefn]
+              | NewtypeDefn Id Id [TypeBinder] Type
+
+data ConstrDefn = ConstrDefn Id [TypeBinder] [AtomType]
+
+data ValueDefn = ValueDefn Id Type Expr
+
+data AtomExpr = VarExpr Id
+              | ConExpr Id
+              | LitExpr Literal
+              | ParExpr Expr
+
+data Expr = AExpr AtomExpr
+          | AppExpr AtomExpr [Arg]
+          | LetExpr [ValueDefn] Expr
+          | CaseExpr AtomType Expr ValueBinder [CaseAlt]
+
+data Arg = TypeArg AtomType
+         | ValueArg AtomExpr
+
+data CaseAlt = ConAlt Id [TypeBinder] [ValueBinder] Expr
+             | LitAlt Literal Expr
+             | DefaultAlt Expr
+
+data TypeBinder = TypeBinder Id Kind
+
+data ValueBinder = ValueBinder Id Type
+
+data Literal = LitInt  Integer  Type
+             | LitChar Char     Type
+             | LitRat  Rational Type
+             | LitStr  String   Type
+
+data AtomType = Tyvar Id
+              | Tycon Id
+              | ParType Type
+
+data BasicType = AType AtomType
+               | AppType BasicType AtomType
+
+data Type = BType BasicType
+          | TypeScheme [TypeBinder] Type
+          | ArrType BasicType Type
+
+data AtomKind = Star
+              | ParKind Kind
+
+data Kind = AKind AtomKind
+          | ArrKind AtomKind Kind
+
+
