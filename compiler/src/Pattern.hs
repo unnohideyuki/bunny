8e86ed6898d4ddd4813bae856c3c84dbe4ab6ce0
UNNO Hideyuki <unno.hideyuki@nifty.com>
2015-09-21 08:33:26 +0900

    Pattern.hs

--- compiler/src/Pattern.hs
+++ compiler/src/Pattern.hs
@@ -56,3 +56,58 @@ subst expr vnew vold =
      Fatbar e1 e2 -> Fatbar (subst e1 vnew vold) (subst e2 vnew vold)
      OtherExpression e -> OtherExpression (subst_expr e)
 
+type Equation = ([Typing.Pat], Expression)
+
+isVar :: Equation -> Bool
+isVar (Typing.PVar _:_, _) = True
+isVar _ = False
+
+isCon :: Equation -> Bool
+isCon (Typing.PCon _ _:_, _) = True
+isCon _ = False
+
+getCon :: Equation -> Typing.Assump
+getCon (Typing.PCon a _:_, _) = a
+getCon _ = error $ "must not happen, getCon"
+
+mkVar :: Int -> Variable
+mkVar k = "_U" ++ show k
+
+partition :: Eq b => (a -> b) -> [a] -> [[a]]
+partition _ [] = []
+partition _ [x] = [[x]]
+partition f (x:x':xs)
+  | f x == f x' = tack x (partition f (x':xs))
+  | otherwise   = [x] : partition f (x':xs)
+  where
+    tack x xss = (x : head xss) : tail xss
+
+match k [] qs def = foldr Fatbar def [e | ([], e) <- qs]
+match k (u:us) qs def =
+  let
+    matchVarCon k us qs def
+      | isVar (head qs) = matchVar k us qs def
+      | isCon (head qs) = matchCon k us qs def
+
+    matchVar k (u:us) qs def =
+      match k us [(ps, subst e u v) | (Typing.PVar v:ps, e) <- qs] def
+
+    matchCon k (u:us) qs def =
+      Case u [matchClause c k (u:us) (choose c qs) def | c <- cs]
+        where cs = constructors (getCon (head qs))
+
+    matchClause c k (u:us) qs def =
+      Clause c us' (match
+                    (k + k)
+                    (us' ++ us)
+                    [(ps' ++ ps, e) | (Typing.PCon c ps':ps, e) <- qs]
+                    def)
+      where
+        k' = arity c
+        us' = [mkVar (i+k) | i <- [1..k']]
+
+    choose c qs = [q | q <-qs, (getCon q) `cequal` c]
+      where
+        (n Typing.:>: _) `cequal` (n' Typing.:>: _) = n == n'
+  in
+   foldr (matchVarCon k (u:us)) def (partition isVar qs)
