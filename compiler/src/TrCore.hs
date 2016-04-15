c903d1854c6150f81e780d4de3957c82f7ba699e
UNNO Hideyuki <unno.hideyuki@nifty.com>
2016-04-15 21:10:17 +0900

    added a comment.

--- compiler/src/TrCore.hs
+++ compiler/src/TrCore.hs
@@ -8,5 +8,9 @@ import qualified Types as Ty
 
 import Debug.Trace
 
+trKind :: Ty.Kind -> Kind
+trKind Ty.Star = Star
+trKind (Ty.Kfun k1 k2) = Kfun (trKind k1) (trKind k2)
+
 trType :: Ty.Type -> Type
 trType _ = TyVarTy (TypeVar "a" Star) -- Dummy value that make the test fail.
