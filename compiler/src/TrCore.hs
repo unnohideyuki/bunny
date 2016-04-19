dde63632a2ddacfbed5a2a9d81d6118a9080bd23
UNNO Hideyuki <unno.hideyuki@nifty.com>
2016-04-19 22:53:15 +0900

    FunTy implemented, and it turns green.

--- compiler/src/TrCore.hs
+++ compiler/src/TrCore.hs
@@ -12,6 +12,11 @@ trKind :: Ty.Kind -> Kind
 trKind Ty.Star = Star
 trKind (Ty.Kfun k1 k2) = Kfun (trKind k1) (trKind k2)
 
+isFunTyCon :: TyCon -> Bool
+isFunTyCon (TyCon "(->)" (Kfun Star (Kfun Star Star))) = True
+isFunTyCon _                                           = False
+
+
 trType :: Ty.Type -> Type
 trType (Ty.TCon (Ty.Tycon n k)) = TyConApp (TyCon n (trKind k)) []
 trType (Ty.TAp t1 t2) =
@@ -20,8 +25,8 @@ trType (Ty.TAp t1 t2) =
     t2' = trType t2
   in
    case t1' of
-     TyConApp tycon ts -> TyConApp tycon (ts ++ [t2'])
-     -- TODO: FynTy
+     TyConApp tycon ts | isFunTyCon tycon && length ts == 1 -> FunTy (head ts) t2'
+                       | otherwise                          -> TyConApp tycon (ts ++ [t2'])
      _                 -> AppTy t1' t2'
 
 trType _ = TyVarTy (TypeVar "a" Star) -- Dummy value that make the test fail.
