8c4b2bda9f90006239c328bbc327df785eeb6912
UNNO Hideyuki <unno.hideyuki@nifty.com>
2016-04-29 21:16:39 +0900

    translation of Pat.Case
    かなりくるしまぎれ。
    さいしょ、expr' = trExpr as ("", expr) のようには書けないと思い、
    zip ns ts ができるローカル環境ないでと思って trpat にしたが、
    ns, ts の情報を as ([Assump]) に追加して、普通に trExpr にまわすべき。
    また、Pattern, Core 双方の Case 式の仕様がよくわかってない。2016-04-29

--- compiler/src/TrCore.hs
+++ compiler/src/TrCore.hs
@@ -24,8 +24,10 @@ trType (Ty.TAp t1 t2) =
     t2' = trType t2
   in
    case t1' of
-     TyConApp tycon ts | isFunTyCon tycon && length ts == 1 -> FunTy (head ts) t2'
-                       | otherwise                          -> TyConApp tycon (ts ++ [t2'])
+     TyConApp tycon ts | isFunTyCon tycon && length ts == 1
+                         -> FunTy (head ts) t2'
+                       | otherwise
+                         -> TyConApp tycon (ts ++ [t2'])
      _                 -> AppTy t1' t2'
 
 -- fix me : the order of ID and [Assump]
@@ -37,7 +39,7 @@ tyLookup n as = let
   in
    case sc of
      Ty.Forall [] ([] Ty.:=> t) -> trType t
-     _                       -> error $ "forall not supported yet: " ++ show sc
+     _ -> error $ "forall not supported yet: " ++ show sc
 
 trExpr :: [Ty.Assump] -> (Id, Pat.Expression) -> Expr
 trExpr as (n, (Pat.Fatbar e Pat.Error)) = trExpr as (n, e)
@@ -67,12 +69,32 @@ trExpr as (n, (Pat.Lambda ns expr)) =
     lam' (v:vs) e = Lam v $ lam' vs e
     lam' [] e = e
 
-    expr' = Var (TypeVar "dummy" Star)
+    trpat (Pat.Case n cs) =
+      let
+        vt = case lookup n $ zip ns ts of
+          Just t -> trType t
+          Nothing -> error $ "variable not found: " ++ n
+        scrut = Var (TermVar n vt)
+        case_bndr = TermVar (n++"b") vt
+        alts = map trclause cs
+      in
+       Case scrut case_bndr alts
+
+    trpat e = trExpr as ("", e)
+
+    trclause (Pat.Clause a@(n Ty.:>: _) vs e) =
+      let
+        t = tyLookup n [a]
+      in
+       (DataAlt (DataCon n [] t), [], trExpr as (n,e))
     {-
-    It can not as "expr' = trExpr as ("", expr)" because
-    ns and ts information is needed to translate the expr here.
-    -- expr' = trpat expr
+    かなりくるしまぎれ。
+    さいしょ、expr' = trExpr as ("", expr) のようには書けないと思い、
+    zip ns ts ができるローカル環境ないでと思って trpat にしたが、
+    ns, ts の情報を as ([Assump]) に追加して、普通に trExpr にまわすべき。
+    また、Pattern, Core 双方の Case 式の仕様がよくわかってない。2016-04-29
     -}
+    expr' = trpat expr
   in
    lam' vs expr'
 
@@ -84,7 +106,8 @@ ptypes t =
  let
    ptypes' ts (Ty.TAp
                (Ty.TAp
-                (Ty.TCon (Ty.Tycon "(->)" (Ty.Kfun Ty.Star (Ty.Kfun Ty.Star Ty.Star))))
+                (Ty.TCon (Ty.Tycon
+                          "(->)" (Ty.Kfun Ty.Star (Ty.Kfun Ty.Star Ty.Star))))
                 t1)
                t2)
      = ptypes' (ts ++ [t1]) t2
