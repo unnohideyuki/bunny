6f87ed73b7f74cdb11e0b14fd9f5f30fab524968
UNNO Hideyuki <unno.hideyuki@nifty.com>
2016-07-04 19:14:17 +0900

    fv: free variables

--- compiler/src/STG.hs
+++ compiler/src/STG.hs
@@ -2,6 +2,9 @@ module STG where
 
 import Prelude hiding((<$>))
 import Text.PrettyPrint.ANSI.Leijen
+import Data.List.Split
+import Data.Char
+import Data.List
 
 import Symbol
 import qualified Core
@@ -26,3 +29,31 @@ data Bind = Bind Var Expr
           deriving Show
 
 type Program = [Bind]
+
+{- Todo: (isUpper.head) may not be enough to detect a module name.
+         eg. When using Unicode name for module.
+-}
+isLocal :: Id -> Bool
+isLocal s =
+  let
+    a = dropWhile (isUpper.head) $ splitOn "." s
+  in
+   (length a > 1) || head s == '_'
+
+-- fv: Free Variables
+fv :: Expr -> [Id]
+
+fv (AtomExpr (LitAtom _)) = []
+
+fv (AtomExpr (VarAtom (TermVar n))) =
+  case isLocal n of
+    True -> [n]
+    False -> []
+
+fv (FunAppExpr f args) = fv f ++ concatMap fv args
+
+fv (LetExpr bs e) = (fv e ++ concatMap fv' bs) \\ concatMap bv bs
+  where
+    fv' (Bind _ e) = fv e
+    bv (Bind (TermVar n) _) = [n]
+
