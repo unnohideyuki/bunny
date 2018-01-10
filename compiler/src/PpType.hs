ca8dff34acc377f297b88f9bcfc0269273741865
UNNO Hideyuki <unno.hideyuki@nifty.com>
2018-01-10 23:12:47 +0900

    improve a little more the printing for --ddump-assump.

--- compiler/src/PpType.hs
+++ compiler/src/PpType.hs
@@ -9,6 +9,8 @@ ppType (TAp (TAp (TCon (Tycon "(->)" (Kfun Star (Kfun Star Star)))) t1) t2) =
 
 ppType (TAp (TCon (Tycon "[]" (Kfun Star Star))) t) = "[" ++ ppType t ++ "]"
 
+ppType (TAp t1 t2) = ppType t1 ++ " " ++ ppType t2
+
 ppType (TVar (Tyvar n Star)) = n
 
 ppType (TCon (Tycon n _)) = n
