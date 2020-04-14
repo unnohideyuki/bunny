f703f456a74a033354d719982b888cc62e231441
Unno Hideyuki <unno.hideyuki@nifty.com>
2020-04-14 21:14:50 +0900

    modified showlogs.sh to see Lexer.x and Parser.y

--- compiler/showlogs.sh
+++ compiler/showlogs.sh
@@ -7,30 +7,38 @@ thints=0
 sp32=`echo -e -n "\t\t\t\t" | expand`
 
 for f in src/*.hs app/Main.hs; do
-    if [ $f != src/Lexer.hs -a $f != src/Parser.hs -a $f != src/unicateg.hs ]; then
-	bn=`basename $f`
-	# file name
-	name=`echo -n -e "$f$sp32" | cut -c 1-32`
-	echo -n -e "$name"
+    bn=`basename $f`
+    # file name
+    name=`echo -n -e "$f$sp32" | cut -c 1-32`
+    if [ $bn = "Lexer.hs" ];then
+	name=`echo -n -e "src/Lexer.x$sp32" | cut -c 1-32`
+    elif [ $bn = "Parser.hs" ];then
+	name=`echo -n -e "src/Parser.y$sp32" | cut -c 1-32`
+    fi
+    echo -n -e "$name"
 
-	# lines
-	n=`wc -l $f | cut -d " " -f 1`
-	echo -n -e "$n lines\t"
-	tlines=`expr $tlines \+ $n`
+    # lines
+    n=`wc -l $f | cut -d " " -f 1`
+    if [ $bn = "Lexer.hs" ];then
+	n=`wc -l src/Lexer.x | cut -d " " -f 1`
+    elif [ $bn = "Parser.hs" ];then
+	n=`wc -l src/Parser.y | cut -d " " -f 1`
+    fi
+    echo -n -e "$n lines\t"
+    tlines=`expr $tlines \+ $n`
 	
-	# warnings
-	w=`cat log/* | grep -E src.$bn | grep "warning:" | wc -l | cut -d " " -f 1`
-	echo -n -e "$w warnings\t"
-	twarns=`expr $twarns \+ $w`
+    # warnings
+    w=`cat log/* | grep -E src.$bn | grep "warning:" | wc -l | cut -d " " -f 1`
+    echo -n -e "$w warnings\t"
+    twarns=`expr $twarns \+ $w`
 
-	# hints
-	h=`hlint -XHaskell2010 $f | tail -1 | cut -d " " -f 1 | sed s/No/0/`
-	thints=`expr $thints \+ $h`
-	if [ $h -gt 1 ];then
-	    echo "$h hints"
-	else
-	    echo "$h hint"
-	fi
+    # hints
+    h=`hlint -XHaskell2010 $f | tail -1 | cut -d " " -f 1 | sed s/No/0/`
+    thints=`expr $thints \+ $h`
+    if [ $h -gt 1 ];then
+	echo "$h hints"
+    else
+	echo "$h hint"
     fi
 done
 
