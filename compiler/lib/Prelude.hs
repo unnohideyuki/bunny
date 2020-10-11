6223f8ff3274d175601a30a4e24ad5288df0f8f9
Unno Hideyuki <unno.hideyuki@nifty.com>
2020-10-11 22:20:51 +0900

    zip3 and zipWith3 (closed #081)

--- compiler/lib/Prelude.hs
+++ compiler/lib/Prelude.hs
@@ -870,13 +870,19 @@ minimum xs       = foldl1 min xs
 zip :: [a] -> [b] -> [(a,b)]
 zip =  zipWith (,)
 
+zip3 :: [a] -> [b] -> [c] -> [(a, b, c)]
+zip3 =  zipWith3 (,,)
+
 zipWith                 :: (a->b->c) -> [a] -> [b] -> [c]
 zipWith z (a:as) (b:bs) =  z a b : zipWith z as bs
 zipWith _ _ _           = []
 
+zipWith3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+zipWith3 z (a:as) (b:bs) (c:cs) = z a b c : zipWith3 z as bs cs
+zipWith3 _ _ _ _                = []
+
 unzip :: [(a, b)] -> ([a], [b])
 unzip =  foldr (\(a, b) (as, bs) -> (a:as, b:bs)) ([], []) -- todo: ~
-
 
 -- PreludeIO
 putStrLn :: String -> IO ()
